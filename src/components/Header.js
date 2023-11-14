2668d404ea11c35f3e2774441cea0cc8b9ff2a24
bundasse <ymtotolove@gmail.com>
2023-11-14 22:30:13 +0900

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+// 리액트 컴포넌트의 이름은 항상 대문자로 할 것.
+function Header() {
+    return (
+        <header>
+        <h1>header</h1>
+        </header>
+    );
+};
+
+export default Header;
+//   익스포트해주는거 잊지말자!
